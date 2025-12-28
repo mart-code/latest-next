981747b51b3cfdd3094d9833d042f95e9c050bd8
mart-code <aamadewoyin@gmail.com>
2025-12-28 19:17:22 +0100

    nextjs 16 course initial commit

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const UserDetails = async ({params}: {params: Promise<{id:string}>}) => {
+    const {id} = await params;
+  return (
+    <div><h1>Showing Details for user #{id}</h1></div>
+  )
+}
+
+export default UserDetails
---
+++ components/Hello.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+const Hello = () => {
+  return (
+    <div>This is a client compinent</div>
+  )
+}
+
+export default Hello
