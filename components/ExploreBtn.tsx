f7ad77c40c4161ba867a6a0f696258093ee1c30f
mart-code <aamadewoyin@gmail.com>
2025-12-31 15:03:40 +0100

    integrated posthog

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,8 +1,17 @@
 'use client';
 import Image from "next/image";
+import posthog from "posthog-js";
+
 const ExploreBtn = () => {
+  const handleClick = () => {
+    posthog.capture('explore_events_clicked', {
+      button_location: 'homepage_hero',
+      destination: '#events',
+    });
+  };
+
   return (
-    <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={()=> console.log('clicked')}>
+    <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={handleClick}>
         <a href="#events">Explore Events <Image src='./icons/arrow-down.svg' width={24} height={24} alt='arrow-down/'/></a></button>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,28 @@
+'use client';
 import Link from "next/link"
 import Image from "next/image"
+import posthog from "posthog-js";
 
 const Navbar = () => {
+  const handleNavClick = (linkName: string, destination: string) => {
+    posthog.capture('nav_link_clicked', {
+      link_name: linkName,
+      destination: destination,
+      nav_section: 'main_navbar',
+    });
+  };
+
   return (
     <header>
         <nav>
-            <Link href="/">
+            <Link href="/" onClick={() => handleNavClick('logo', '/')}>
             <Image src={'/icons/logo.png'} alt="Logo" width={24} height={24}/>
             <p>DevEvents</p>
             </Link>
             <ul>
-                <Link href={'/'}>Home</Link>
-                <Link href={'/'}>Events</Link>
-                <Link href={'/'}>Create Events</Link>
+                <Link href={'/'} onClick={() => handleNavClick('Home', '/')}>Home</Link>
+                <Link href={'/'} onClick={() => handleNavClick('Events', '/')}>Events</Link>
+                <Link href={'/'} onClick={() => handleNavClick('Create Events', '/')}>Create Events</Link>
             </ul>
         </nav>
     </header>
